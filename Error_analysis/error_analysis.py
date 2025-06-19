28086294bf5f03d5373188a5609d51dcc5a14efd
Diego <diego.picello@mail.polimi.it>
2025-06-19 21:19:39 +0200

    Error analysis script.

--- Error_analysis/error_analysis.py
+++ Error_analysis/error_analysis.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import glob
 
+DISCARD_OUTLIERS = True
+OUTLIERS_THRESHOLD = 1000
+
+
 file_paths = glob.glob('Datasets/**/*.csv', recursive=True)
 dfs = [
     pd.read_csv(f,
@@ -13,15 +17,37 @@ big_df = pd.concat(dfs, ignore_index=True)
 data = big_df.to_numpy()
 
 errors = data[:, 1] - data[:, 3]
-#errors = np.abs(errors)
+if DISCARD_OUTLIERS:
+    mask = np.abs(errors) < OUTLIERS_THRESHOLD
+    errors = errors[mask]
 
-max_err = errors.max()
-print(errors.mean())
-print(errors.var())
-print(errors.min(), max_err)
+abs_err = np.abs(errors)
 
 
-for i in range(len(errors)):
-    if errors[i] == max_err:
-        print(i)
-        print(data[i])
+# General metrics
+print("General metrics:")
+print("Number of samples:", errors.shape[0])
+print(f"Mean: {errors.mean():.1f}  Absolute error mean: {abs_err.mean():.1f}")
+print(f"Variance: {errors.var():.1f}  Standard deviation: {np.sqrt(errors.var()):.1f}")
+print("Min error:", errors.min(), "Max error:", errors.max())
+print("Min absolute error:", abs_err.min(), "Max absolute error:", abs_err.max())
+print()
+
+# Per distance metrics
+for dist in (940, 1125, 1180, 1220, 1550, 1595):
+    print(dist, ":")
+    dist_data = data[data[:, 3] == dist]
+
+    dist_errors = dist_data[:, 1] - dist_data[:, 3]
+    if DISCARD_OUTLIERS:
+        mask = np.abs(dist_errors) < OUTLIERS_THRESHOLD
+        dist_errors = dist_errors[mask]
+
+    abs_dist_err = np.abs(dist_errors)
+
+    print("Number of samples:", dist_errors.shape[0])
+    print(f"Mean: {dist_errors.mean():.1f}  Absolute error mean: {abs_dist_err.mean():.1f}")
+    print(f"Variance: {dist_errors.var():.1f}  Standard deviation: {np.sqrt(dist_errors.var()):.1f}")
+    print("Min error:", dist_errors.min(), "Max error:", dist_errors.max())
+    print("Min absolute error:", abs_dist_err.min(), "Max absolute error:", abs_dist_err.max())
+    print()
